3c32135206b6c9e6040ed5fe90469a9cbe935c50
Nguyễn Hoàng Nam <hnamhocit@gmail.com>
2024-12-11 15:43:43 +0700

    Update TeachersSection

--- src/components/TeachersSection/index.tsx
+++ src/components/TeachersSection/index.tsx
@@ -1,29 +1,32 @@
+import {motion} from "motion/react"
+
+const sections = [
+	{ name: 'Lãnh đạo đơn vị', 
+	 items: ['Tổng số giáo viên, nhân viên đầu năm học 2021-2022 là 81 người. Trong đó:', 
+			 'Bộ phận hành chính, bảo vệ, phục vụ: 11 người (Đại học: 05).', 
+			 'Giáo viên trực tiếp tham gia dạy lớp: 64 người, trong đó 100% giáo viên đạt chuẩn, 17/64 giáo viên là Thạc sĩ.']}
+	{title: 'Tổ Toán: 11 giáo viên', items: ['Tổ trưởng chuyên môn: Thầy Phạm Đỗ Hải.', 'Tổ phó chuyên môn: Cô Nguyễn Thị Phương Thảo.']},
+	{ title: 'Tổ Văn: 09 giáo viên', items: ['Tổ trưởng chuyên môn: Thầy Nông Ngọc Trọng.', 'Tổ phó chuyên môn: Cô Nguyễn Thanh Hiền.'] },
+	{ title: 'Tổ Ngoại ngữ: 08 giáo viên.', items: ['Tổ trưởng chuyên môn: Cô Võ Thị Phượng.', 'Tổ phó chuyên môn: Cô Trần Thị Tường Vi.'] },
+	{ title: 'Tổ Lý: 06 giáo viên.', items: ['Tổ trưởng chuyên môn: Cô Nguyễn Hoài Thu', 'Tổ phó chuyên môn: Thầy Đoàn Minh Phong'] },
+	{ title: 'Tổ Hóa: 07 giáo viên', items: ['Tổ trưởng chuyên môn: Cô Đinh Kim Cúc', 'Tổ phó chuyên môn: Cô Đào Thị Hưng Minh'] },
+	{ title: 'Tổ Sinh-Kỹ thuật nông nghiệp: 05 giáo viên', items: ['Tổ trưởng chuyên môn: Cô Bùi Thị Lan Anh'] },
+	{ title: 'Tổ Sử-Địa-Giáo dục công dân: 08 giáo viên', items: ['Tổ trưởng chuyên môn: Thầy Nguyễn Văn Bé', '- Tổ phó chuyên môn: Thầy Vũ Văn Quyết'] },
+	{ title: 'Tổ Tin học-Kỹ thuật công nghiệp: 06 giáo viên', items: ['Tổ trưởng chuyên môn: Thầy Lê Kỳ Nhân'] },
+	{ title: 'Tổ Giáo dục thể chất-Quốc phòng an ninh: 06 giáo viên', items: ['Tổ trưởng chuyên môn: Thầy Nguyễn Văn Của', 'Tổ phó chuyên môn: Thầy Phạm Việt Diệp'] },
+	{ title: 'Tổ Hành chính', items: ['Tổ trưởng: Cô Nguyễn Thị Bích Liên', 'Tổ phó: Thầy Phạm Văn Tiến'] },
+]
+
 const TeachersSection = () => {
 	return (
-		<div>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec
-			porta lectus. Etiam tempus sit amet tortor sed tincidunt. Morbi
-			ultrices dolor et ipsum fringilla, bibendum convallis mauris
-			rhoncus. Fusce nec tincidunt erat. Mauris a nisi sit amet lectus
-			tempus feugiat. In quis libero commodo, viverra orci sed, vehicula
-			lacus. Integer dolor ipsum, efficitur id commodo vel, euismod eu mi.
-			Phasellus laoreet sed purus ut imperdiet. Sed lobortis vitae felis
-			vel tempor. Etiam bibendum, massa vitae pulvinar consectetur, arcu
-			ante maximus eros, nec ullamcorper libero est consectetur mauris.
-			Nunc vel ante non ex ullamcorper finibus. Proin placerat elementum
-			felis, vel congue risus laoreet nec. Pellentesque tortor dolor,
-			pharetra mollis tristique ut, fermentum in sapien. Ut neque nibh,
-			auctor vel mauris a, fermentum vulputate mauris. Duis faucibus
-			turpis ut nisl bibendum, sit amet aliquam arcu feugiat. Ut molestie
-			commodo bibendum. Curabitur eget posuere ante. Aliquam id ornare
-			ante. Duis lorem lacus, scelerisque ac nibh sed, tincidunt sodales
-			est. Vestibulum at imperdiet quam. Proin eget turpis rutrum, finibus
-			justo in, egestas diam. Mauris mi dui, congue et aliquet at, semper
-			at purus. Aliquam vulputate imperdiet dui, quis mattis leo pretium
-			sit amet. Quisque ornare tortor neque, in venenatis nibh mollis in.
-			Cras blandit sit amet lorem suscipit tristique. Sed malesuada in
-			ligula at interdum. Suspendisse sagittis maximus suscipit. Lorem
-			ipsum dolor sit amet, consectetur adipiscing elit.
+		<div className="grid grid-cols-3 gap-5">
+			{sections.map(section => <motion.div className="space-y-3 p-4 shadow-md border rounded-2xl" key={section.name} initial={{ opacity: 0, scale: 0 }} whileInView={{ opacity: 1, scale: 1 }}>
+				<div className="text-xl font-semibold">{section.name}</div>
+				{section.items.map(item => {
+					const [label, text] = item.split(':')
+					return <div><span className="font-medium">{label}</span> <span className="underline decoration-pink-500">{text}</span></div>
+				})}
+			</motion.div>)}
 		</div>
 	);
 };
